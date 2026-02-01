634f375485cb3b5ffdaafd2a32bcbfb52c168e5c
Ryan Waits <ryan.waits@gmail.com>
2026-02-01 14:57:09 -0600

    feat: remove packages/react

--- packages/react/src/components/styled/sections/index.ts
+++
@@ -1,8 +0,0 @@
-// Section components for single-page API reference
-
-export { ClassSection, type ClassSectionProps } from './ClassSection';
-export { EnumSection, type EnumSectionProps } from './EnumSection';
-export { ExportSection, type ExportSectionProps } from './ExportSection';
-export { FunctionSection, type FunctionSectionProps } from './FunctionSection';
-export { InterfaceSection, type InterfaceSectionProps } from './InterfaceSection';
-export { VariableSection, type VariableSectionProps } from './VariableSection';
